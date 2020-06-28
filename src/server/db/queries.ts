2eedae459c9789e0d5d8700728c721173db85ee9
Sarp ISIK <wdsarpisik@gmail.com>
2020-06-28 19:49:36 +0200

    Add query users table

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -29,10 +29,20 @@ export default [
         content text NOT NULL
     )`,
 
+    // Create users table
+    `CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        name varchar(255) NOT NULL,
+        email varchar(255) NOT NULL,
+        created_at TIMESTAMPTZ DEFAULT Now()
+    )`,
+
     // Create comments table
     `CREATE TABLE IF NOT EXISTS comments (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         created_at TIMESTAMPTZ DEFAULT Now(),
-        post_id UUID REFERENCES posts(id) ON DELETE CASCADE
+        users_id UUID REFERENCES userss(id) ON DELETE CASCADE,
+        post_id UUID REFERENCES posts(id) ON DELETE CASCADE,
+        content text NOT NULL
     )`,
 ];
