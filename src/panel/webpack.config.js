581b5b4b027cac7c94326af7d97bda36af5502c2
Sarp ISIK <wdsarpisik@gmail.com>
2020-06-30 08:22:47 +0200

    Create init webpack config file

---
+++ src/panel/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const mode = isProd ? 'production' : 'development';
+
+module.exports = {
+    entry: './src/components/index.tsx',
+    target: 'web',
+    devtool: 'inline-source-map',
+    mode,
+    output: {
+        path: path.resolve(__dirname, '..', 'server', 'public'),
+        filename: isProd ? 'scripts/[name].[chunkhash].bundle.js' : '[name].js',
+        publicPath: '/panel',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { hmr: !isProd },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(
+                __dirname,
+                'src',
+                'components',
+                'index.html'
+            ),
+        }),
+        new MiniCssExtractPlugin({
+            filename: isProd ? 'stylesheets/[name].[hash].css' : '[name].css',
+            chunkFilename: isProd ? 'stylesheets/[id].[hash].css' : '[id].css',
+        }),
+    ],
+};
