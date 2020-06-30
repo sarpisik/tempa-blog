aeee72adca4c15c24effbabb6706221731459225
Sarp ISIK <wdsarpisik@gmail.com>
2020-06-30 08:24:22 +0200

    Add HtmlWebpackPlugin

--- src/panel/webpack.config.js
+++ src/panel/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 const isProd = process.env.NODE_ENV === 'production';
 const mode = isProd ? 'production' : 'development';
