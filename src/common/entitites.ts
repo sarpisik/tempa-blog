37f7acf10f845cb8d6041e163d2c5f54ddeee459
Sarp ISIK <wdsarpisik@gmail.com>
2020-07-07 13:06:42 +0200

    Create type ImageFormats

--- src/common/entitites.ts
+++ src/common/entitites.ts
@@ -1,10 +1,15 @@
+export interface IAuthor extends PreAuthor {
+    id: string;
+    created_at: string;
+}
 export interface PreAuthor {
     name: string;
     email: string;
     bio: string;
-    avatar_url: string;
+    avatar_url: ImageFormats;
 }
-export interface IAuthor extends PreAuthor {
-    id: string;
-    created_at: string;
+interface ImageFormats {
+    src: string;
+    lqip: string;
+    webp: string;
 }
